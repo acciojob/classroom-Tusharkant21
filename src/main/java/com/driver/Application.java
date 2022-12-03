629092fb9bbc34e0cabb4f70b1af699a058bf712
Tusharkant21 <64279043+Tusharkant21@users.noreply.github.com>
2022-12-03 17:33:13 +0530

    2nd attempt

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -8,6 +8,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class Application {
 
+	//main class
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
